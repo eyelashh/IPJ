2d83765b32ba70fccfcd7100bc6db6ae7c956dba
tamarabarros92 <tamarabarros@hotmail.com>
2018-12-19 18:18:37 +0000

    hfgf

--- Projecto_ViewComics/src/ViewComics.java
+++ Projecto_ViewComics/src/ViewComics.java
@@ -1,10 +1,16 @@
+import javax.swing.JFrame;
 
 public class ViewComics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
+		InterfaceGrafica ig = new InterfaceGrafica();
+
+		ig.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		ig.setBounds(100, 100, 1280, 780);
+		ig.pack();
+		ig.setVisible(true);
 
 	}
 
