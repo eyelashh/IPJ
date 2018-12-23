1d73bdc2969ab8f657c9f129f04142684d1d262e
tamarabarros92 <tamarabarros@hotmail.com>
2018-12-23 19:22:52 +0000

    jjj

--- Projecto_JavaBank/src/BancoAppAdm.java
+++ Projecto_JavaBank/src/BancoAppAdm.java
@@ -61,8 +61,8 @@ public class BancoAppAdm {
 
 		try {
 			BancoAppAdm window = new BancoAppAdm();
-
 			window.frame.setVisible(true);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -180,6 +180,118 @@ public class BancoAppAdm {
 		// Painel da funcionario da parte administrador
 		JPanel JPAdmFuncionario = new JPanel();
 		JPAdmFuncionario.setVisible(false);
+
+		// Painel da gestao do administrador
+		JPanel JPAdmGestao = new JPanel();
+		JPAdmGestao.setBounds(0, 0, 1042, 576);
+		JPAdm.add(JPAdmGestao);
+		JPAdmGestao.setLayout(null);
+
+		// linha que separa na parte da gestao
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(Color.BLACK);
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		separator_1.setAlignmentX(Component.LEFT_ALIGNMENT);
+		separator_1.setBounds(483, 68, 29, 433);
+		JPAdmGestao.add(separator_1);
+
+		// texto : username
+		JLabel lblUsername_1 = new JLabel("Username :");
+		lblUsername_1.setVerifyInputWhenFocusTarget(false);
+		lblUsername_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblUsername_1.setBounds(144, 81, 131, 41);
+		JPAdmGestao.add(lblUsername_1);
+
+		// campo de texto na gestão da parte do administrador
+		lblAdminGestUsername = new JTextField();
+		lblAdminGestUsername.setBounds(144, 134, 271, 33);
+		JPAdmGestao.add(lblAdminGestUsername);
+
+		// texto : password
+		JLabel lblPassword_1 = new JLabel("Password :");
+		lblPassword_1.setVerifyInputWhenFocusTarget(false);
+		lblPassword_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblPassword_1.setBounds(144, 178, 131, 41);
+		JPAdmGestao.add(lblPassword_1);
+
+		// campo de texto na gestão da parte do administrador
+		textAdminGestPass = new JTextField();
+		textAdminGestPass.setColumns(10);
+		textAdminGestPass.setBounds(144, 230, 271, 33);
+		JPAdmGestao.add(textAdminGestPass);
+
+		// texto : novausername
+		JLabel textFieldAdminGestUsername = new JLabel("Nova Username :");
+		textFieldAdminGestUsername.setVerifyInputWhenFocusTarget(false);
+		textFieldAdminGestUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		textFieldAdminGestUsername.setBounds(144, 275, 191, 41);
+		JPAdmGestao.add(textFieldAdminGestUsername);
+
+		// campo de texto na gestão da parte do administrador
+		textFieldAdminGestNovaUser = new JTextField();
+		textFieldAdminGestNovaUser.setColumns(10);
+		textFieldAdminGestNovaUser.setBounds(144, 327, 271, 33);
+		JPAdmGestao.add(textFieldAdminGestNovaUser);
+
+		// botao de confimar - submete a nova username
+		JButton btnAdminGestConfirmar = new JButton("Confirmar");
+		btnAdminGestConfirmar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		btnAdminGestConfirmar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		btnAdminGestConfirmar.setBounds(144, 415, 131, 41);
+		JPAdmGestao.add(btnAdminGestConfirmar);
+
+		// botao de cancelar - limpar
+		JButton btnAdminGestCancelar = new JButton("Cancelar");
+		btnAdminGestCancelar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		btnAdminGestCancelar.setBounds(294, 415, 131, 41);
+		JPAdmGestao.add(btnAdminGestCancelar);
+
+		textFieldAdminGestPass1 = new JTextField();
+		textFieldAdminGestPass1.setColumns(10);
+		textFieldAdminGestPass1.setBounds(579, 134, 271, 33);
+		JPAdmGestao.add(textFieldAdminGestPass1);
+
+		JLabel lblPassword_2 = new JLabel("Password :");
+		lblPassword_2.setVerifyInputWhenFocusTarget(false);
+		lblPassword_2.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblPassword_2.setBounds(579, 81, 131, 41);
+		JPAdmGestao.add(lblPassword_2);
+
+		JLabel lblNovaPassword = new JLabel("Nova Password :");
+		lblNovaPassword.setVerifyInputWhenFocusTarget(false);
+		lblNovaPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblNovaPassword.setBounds(579, 178, 168, 41);
+		JPAdmGestao.add(lblNovaPassword);
+
+		textFieldAdminGestNovaPass = new JTextField();
+		textFieldAdminGestNovaPass.setColumns(10);
+		textFieldAdminGestNovaPass.setBounds(579, 230, 271, 33);
+		JPAdmGestao.add(textFieldAdminGestNovaPass);
+
+		JLabel lblConfirmarNovaUsername = new JLabel("Confirmar Password :");
+		lblConfirmarNovaUsername.setVerifyInputWhenFocusTarget(false);
+		lblConfirmarNovaUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblConfirmarNovaUsername.setBounds(579, 275, 271, 41);
+		JPAdmGestao.add(lblConfirmarNovaUsername);
+
+		textFieldAdminGestConf = new JTextField();
+		textFieldAdminGestConf.setColumns(10);
+		textFieldAdminGestConf.setBounds(579, 327, 271, 33);
+		JPAdmGestao.add(textFieldAdminGestConf);
+
+		JButton buttonAdminGestConf = new JButton("Confirmar");
+		buttonAdminGestConf.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		buttonAdminGestConf.setBounds(579, 415, 131, 41);
+		JPAdmGestao.add(buttonAdminGestConf);
+
+		JButton buttonAdminGestCan = new JButton("Cancelar");
+		buttonAdminGestCan.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		buttonAdminGestCan.setBounds(729, 415, 131, 41);
+		JPAdmGestao.add(buttonAdminGestCan);
 		JPAdmFuncionario.setLayout(null);
 		JPAdmFuncionario.setBounds(0, 0, 1042, 576);
 		JPAdm.add(JPAdmFuncionario);
@@ -358,124 +470,13 @@ public class BancoAppAdm {
 		textAdmFunPass.setBounds(463, 456, 225, 31);
 		JPAdmFuncionario.add(textAdmFunPass);
 
-		// Painel da gestao do administrador
-		JPanel JPAdmGestao = new JPanel();
-		JPAdmGestao.setLayout(null);
-		JPAdmGestao.setBounds(0, 0, 1042, 576);
-		JPAdm.add(JPAdmGestao);
-
-		// linha que separa na parte da gestao
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setForeground(Color.BLACK);
-		separator_1.setOrientation(SwingConstants.VERTICAL);
-		separator_1.setAlignmentX(Component.LEFT_ALIGNMENT);
-		separator_1.setBounds(483, 68, 29, 433);
-		JPAdmGestao.add(separator_1);
-
-		// texto : username
-		JLabel lblUsername_1 = new JLabel("Username :");
-		lblUsername_1.setVerifyInputWhenFocusTarget(false);
-		lblUsername_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblUsername_1.setBounds(144, 81, 131, 41);
-		JPAdmGestao.add(lblUsername_1);
-
-		// campo de texto na gestão da parte do administrador
-		lblAdminGestUsername = new JTextField();
-		lblAdminGestUsername.setBounds(144, 134, 271, 33);
-		JPAdmGestao.add(lblAdminGestUsername);
-
-		// texto : password
-		JLabel lblPassword_1 = new JLabel("Password :");
-		lblPassword_1.setVerifyInputWhenFocusTarget(false);
-		lblPassword_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblPassword_1.setBounds(144, 178, 131, 41);
-		JPAdmGestao.add(lblPassword_1);
-
-		// campo de texto na gestão da parte do administrador
-		textAdminGestPass = new JTextField();
-		textAdminGestPass.setColumns(10);
-		textAdminGestPass.setBounds(144, 230, 271, 33);
-		JPAdmGestao.add(textAdminGestPass);
-
-		// texto : novausername
-		JLabel textFieldAdminGestUsername = new JLabel("Nova Username :");
-		textFieldAdminGestUsername.setVerifyInputWhenFocusTarget(false);
-		textFieldAdminGestUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		textFieldAdminGestUsername.setBounds(144, 275, 191, 41);
-		JPAdmGestao.add(textFieldAdminGestUsername);
-
-		// campo de texto na gestão da parte do administrador
-		textFieldAdminGestNovaUser = new JTextField();
-		textFieldAdminGestNovaUser.setColumns(10);
-		textFieldAdminGestNovaUser.setBounds(144, 327, 271, 33);
-		JPAdmGestao.add(textFieldAdminGestNovaUser);
-
-		// botao de confimar - submete a nova username
-		JButton btnAdminGestConfirmar = new JButton("Confirmar");
-		btnAdminGestConfirmar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		btnAdminGestConfirmar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		btnAdminGestConfirmar.setBounds(144, 415, 131, 41);
-		JPAdmGestao.add(btnAdminGestConfirmar);
-
-		// botao de cancelar - limpar
-		JButton btnAdminGestCancelar = new JButton("Cancelar");
-		btnAdminGestCancelar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		btnAdminGestCancelar.setBounds(294, 415, 131, 41);
-		JPAdmGestao.add(btnAdminGestCancelar);
-
-		textFieldAdminGestPass1 = new JTextField();
-		textFieldAdminGestPass1.setColumns(10);
-		textFieldAdminGestPass1.setBounds(579, 134, 271, 33);
-		JPAdmGestao.add(textFieldAdminGestPass1);
-
-		JLabel lblPassword_2 = new JLabel("Password :");
-		lblPassword_2.setVerifyInputWhenFocusTarget(false);
-		lblPassword_2.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblPassword_2.setBounds(579, 81, 131, 41);
-		JPAdmGestao.add(lblPassword_2);
-
-		JLabel lblNovaPassword = new JLabel("Nova Password :");
-		lblNovaPassword.setVerifyInputWhenFocusTarget(false);
-		lblNovaPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblNovaPassword.setBounds(579, 178, 168, 41);
-		JPAdmGestao.add(lblNovaPassword);
-
-		textFieldAdminGestNovaPass = new JTextField();
-		textFieldAdminGestNovaPass.setColumns(10);
-		textFieldAdminGestNovaPass.setBounds(579, 230, 271, 33);
-		JPAdmGestao.add(textFieldAdminGestNovaPass);
-
-		JLabel lblConfirmarNovaUsername = new JLabel("Confirmar Password :");
-		lblConfirmarNovaUsername.setVerifyInputWhenFocusTarget(false);
-		lblConfirmarNovaUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		lblConfirmarNovaUsername.setBounds(579, 275, 271, 41);
-		JPAdmGestao.add(lblConfirmarNovaUsername);
-
-		textFieldAdminGestConf = new JTextField();
-		textFieldAdminGestConf.setColumns(10);
-		textFieldAdminGestConf.setBounds(579, 327, 271, 33);
-		JPAdmGestao.add(textFieldAdminGestConf);
-
-		JButton buttonAdminGestConf = new JButton("Confirmar");
-		buttonAdminGestConf.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		buttonAdminGestConf.setBounds(579, 415, 131, 41);
-		JPAdmGestao.add(buttonAdminGestConf);
-
-		JButton buttonAdminGestCan = new JButton("Cancelar");
-		buttonAdminGestCan.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		buttonAdminGestCan.setBounds(729, 415, 131, 41);
-		JPAdmGestao.add(buttonAdminGestCan);
-
 		// Painel da estatistica da parte administrador
 		JPanel JPAdmEstatistica = new JPanel();
 		JPAdmEstatistica.setVisible(false);
 		JPAdmEstatistica.setBounds(0, 0, 1042, 576);
 		JPAdm.add(JPAdmEstatistica);
 		JPAdmEstatistica.setLayout(null);
+		
 
 		// Painel do cliente da parte administrador
 		JPanel JPAdmCliente = new JPanel();
--- Projecto_JavaBank/src/BancoAppClt.java
+++ Projecto_JavaBank/src/BancoAppClt.java
@@ -1,12 +1,17 @@
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.GrayFilter;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -31,6 +36,15 @@ public class BancoAppClt {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
 
 	/**
 	 * Launch the application.
@@ -41,6 +55,7 @@ public class BancoAppClt {
 		try {
 			BancoAppClt window = new BancoAppClt();
 			window.frame.setVisible(true);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -146,68 +161,236 @@ public class BancoAppClt {
 		JpanelPrincipal.setBounds(198, 142, 1065, 598);
 		frame.getContentPane().add(JpanelPrincipal);
 		JpanelPrincipal.setLayout(null);
-				
-						// Painel principal gestao
-						JPanel JPCltGestao = new JPanel();
-						JPCltGestao.setBounds(16, 16, 1032, 563);
-						JpanelPrincipal.add(JPCltGestao);
+
+		// Painel principal cartao
+		JPanel JPCltCartao = new JPanel();
+		JPCltCartao.setBounds(16, 16, 1032, 563);
+		JpanelPrincipal.add(JPCltCartao);
+		JPCltCartao.setLayout(null);
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(581, 135, 249, 39);
+		JPCltCartao.add(comboBox);
+
+		textField_9 = new JTextField();
+		textField_9.setBounds(581, 178, 250, 31);
+		JPCltCartao.add(textField_9);
+
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(581, 69, 249, 39);
+		JPCltCartao.add(comboBox_1);
+
+		JLabel lblNomeAGravar = new JLabel("Nome a gravar");
+		lblNomeAGravar.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblNomeAGravar.setBounds(591, 251, 229, 23);
+		JPCltCartao.add(lblNomeAGravar);
+
+		textField_10 = new JTextField();
+		textField_10.setBounds(601, 279, 229, 31);
+		JPCltCartao.add(textField_10);
+
+		JLabel lblCdigoDigitos = new JLabel("Código 3 digitos:");
+		lblCdigoDigitos.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblCdigoDigitos.setBounds(591, 327, 172, 23);
+		JPCltCartao.add(lblCdigoDigitos);
+
+		textField_11 = new JTextField();
+		textField_11.setBounds(603, 351, 227, 30);
+		JPCltCartao.add(textField_11);
+
+		JButton btnPedirCarto = new JButton("Pedir Cartão");
+		btnPedirCarto.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		btnPedirCarto.setBounds(583, 416, 120, 38);
+		JPCltCartao.add(btnPedirCarto);
+
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		btnLimpar.setBounds(727, 417, 120, 38);
+		JPCltCartao.add(btnLimpar);
 		
-				// Painel principal transferencia
-				JPanel JPCltTransf = new JPanel();
-				JPCltTransf.setBounds(16, 16, 1032, 563);
-				JpanelPrincipal.add(JPCltTransf);
-				JPCltTransf.setLayout(null);
-				
-						// Box pesquisa da conta do cliente
-						JComboBox comboBoxCLTPesquisa = new JComboBox();
-						comboBoxCLTPesquisa.setBounds(211, 67, 249, 39);
-						JPCltTransf.add(comboBoxCLTPesquisa);
-						
-								JLabel labelClt = new JLabel("Saldo:");
-								labelClt.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
-								labelClt.setBounds(550, 67, 66, 16);
-								JPCltTransf.add(labelClt);
-								
-										textFieldCltSaldo2 = new JTextField();
-										textFieldCltSaldo2.setBounds(560, 95, 185, 30);
-										JPCltTransf.add(textFieldCltSaldo2);
-										
-												textField = new JTextField();
-												textField.setBounds(367, 204, 185, 30);
-												JPCltTransf.add(textField);
-												
-														JLabel lblMontante = new JLabel("Montante:");
-														lblMontante.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
-														lblMontante.setBounds(357, 176, 131, 16);
-														JPCltTransf.add(lblMontante);
-														
-																textField_1 = new JTextField();
-																textField_1.setBounds(367, 285, 287, 30);
-																JPCltTransf.add(textField_1);
-																
-																		JLabel lblContaDestino = new JLabel("Conta Destino:");
-																		lblContaDestino.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
-																		lblContaDestino.setBounds(357, 257, 147, 16);
-																		JPCltTransf.add(lblContaDestino);
-																		
-																				textField_2 = new JTextField();
-																				textField_2.setBounds(367, 367, 185, 30);
-																				JPCltTransf.add(textField_2);
-																				
-																						JLabel lblDataDaOperao = new JLabel("Data da Operação:");
-																						lblDataDaOperao.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
-																						lblDataDaOperao.setBounds(357, 339, 190, 16);
-																						JPCltTransf.add(lblDataDaOperao);
-																						
-																								JButton btnConfirmar = new JButton("Confirmar");
-																								btnConfirmar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-																								btnConfirmar.setBounds(337, 435, 120, 38);
-																								JPCltTransf.add(btnConfirmar);
-																								
-																										JButton button_1 = new JButton("Limpar");
-																										button_1.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-																										button_1.setBounds(509, 435, 120, 38);
-																										JPCltTransf.add(button_1);
+		JLabel lblCartoVerde = new JLabel("Cartão Verde");
+		lblCartoVerde.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblCartoVerde.setBounds(116, 341, 151, 16);
+		JPCltCartao.add(lblCartoVerde);
+		
+		JLabel lblValidadeAnos = new JLabel("Validade: 5 anos");
+		lblValidadeAnos.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblValidadeAnos.setBounds(116, 371, 151, 16);
+		JPCltCartao.add(lblValidadeAnos);
+		
+		JLabel lblRedeVisaE = new JLabel("Rede: Visa e Multibanco");
+		lblRedeVisaE.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblRedeVisaE.setBounds(116, 402, 264, 16);
+		JPCltCartao.add(lblRedeVisaE);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("/Users/tamarabarros/Desktop/cartao34.jpg"));
+		lblNewLabel_1.setBounds(99, 93, 335, 207);
+		JPCltCartao.add(lblNewLabel_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setOrientation(SwingConstants.VERTICAL);
+		separator_2.setForeground(Color.BLACK);
+		separator_2.setAlignmentX(0.0f);
+		separator_2.setBounds(509, 69, 29, 433);
+		JPCltCartao.add(separator_2);
+
+		
+
+		// Painel principal gestao
+		JPanel JPCltGestao = new JPanel();
+		JPCltGestao.setBounds(16, 16, 1032, 563);
+		JpanelPrincipal.add(JPCltGestao);
+		JPCltGestao.setLayout(null);
+
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		separator_1.setForeground(Color.BLACK);
+		separator_1.setAlignmentX(0.0f);
+		separator_1.setBounds(483, 68, 29, 433);
+		JPCltGestao.add(separator_1);
+
+		JLabel label = new JLabel("Password :");
+		label.setVerifyInputWhenFocusTarget(false);
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label.setBounds(570, 95, 131, 41);
+		JPCltGestao.add(label);
+
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(570, 148, 271, 33);
+		JPCltGestao.add(textField_3);
+
+		JLabel label_1 = new JLabel("Nova Password :");
+		label_1.setVerifyInputWhenFocusTarget(false);
+		label_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label_1.setBounds(570, 192, 168, 41);
+		JPCltGestao.add(label_1);
+
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(570, 244, 271, 33);
+		JPCltGestao.add(textField_4);
+
+		JLabel label_2 = new JLabel("Confirmar Password :");
+		label_2.setVerifyInputWhenFocusTarget(false);
+		label_2.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label_2.setBounds(570, 289, 271, 41);
+		JPCltGestao.add(label_2);
+
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(570, 341, 271, 33);
+		JPCltGestao.add(textField_5);
+
+		JButton button = new JButton("Confirmar");
+		button.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		button.setBounds(570, 429, 131, 41);
+		JPCltGestao.add(button);
+
+		JButton button_2 = new JButton("Cancelar");
+		button_2.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		button_2.setBounds(720, 429, 131, 41);
+		JPCltGestao.add(button_2);
+
+		JButton button_3 = new JButton("Cancelar");
+		button_3.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		button_3.setBounds(285, 429, 131, 41);
+		JPCltGestao.add(button_3);
+
+		JButton button_4 = new JButton("Confirmar");
+		button_4.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		button_4.setBounds(135, 429, 131, 41);
+		JPCltGestao.add(button_4);
+
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(135, 341, 271, 33);
+		JPCltGestao.add(textField_6);
+
+		JLabel label_3 = new JLabel("Nova Username :");
+		label_3.setVerifyInputWhenFocusTarget(false);
+		label_3.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label_3.setBounds(135, 289, 191, 41);
+		JPCltGestao.add(label_3);
+
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(135, 244, 271, 33);
+		JPCltGestao.add(textField_7);
+
+		JLabel label_4 = new JLabel("Password :");
+		label_4.setVerifyInputWhenFocusTarget(false);
+		label_4.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label_4.setBounds(135, 192, 131, 41);
+		JPCltGestao.add(label_4);
+
+		textField_8 = new JTextField();
+		textField_8.setBounds(135, 148, 271, 33);
+		JPCltGestao.add(textField_8);
+
+		JLabel label_5 = new JLabel("Username :");
+		label_5.setVerifyInputWhenFocusTarget(false);
+		label_5.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label_5.setBounds(135, 95, 131, 41);
+		JPCltGestao.add(label_5);
+
+		// Painel principal transferencia
+		JPanel JPCltTransf = new JPanel();
+		JPCltTransf.setBounds(16, 16, 1032, 563);
+		JpanelPrincipal.add(JPCltTransf);
+		JPCltTransf.setLayout(null);
+
+		// Box pesquisa da conta do cliente
+		JComboBox comboBoxCLTPesquisa = new JComboBox();
+		comboBoxCLTPesquisa.setBounds(211, 67, 249, 39);
+		JPCltTransf.add(comboBoxCLTPesquisa);
+
+		JLabel labelClt = new JLabel("Saldo:");
+		labelClt.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		labelClt.setBounds(550, 67, 66, 16);
+		JPCltTransf.add(labelClt);
+
+		textFieldCltSaldo2 = new JTextField();
+		textFieldCltSaldo2.setBounds(560, 95, 185, 30);
+		JPCltTransf.add(textFieldCltSaldo2);
+
+		textField = new JTextField();
+		textField.setBounds(367, 204, 185, 30);
+		JPCltTransf.add(textField);
+
+		JLabel lblMontante = new JLabel("Montante:");
+		lblMontante.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblMontante.setBounds(357, 176, 131, 16);
+		JPCltTransf.add(lblMontante);
+
+		textField_1 = new JTextField();
+		textField_1.setBounds(367, 285, 287, 30);
+		JPCltTransf.add(textField_1);
+
+		JLabel lblContaDestino = new JLabel("Conta Destino:");
+		lblContaDestino.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblContaDestino.setBounds(357, 257, 147, 16);
+		JPCltTransf.add(lblContaDestino);
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(367, 367, 185, 30);
+		JPCltTransf.add(textField_2);
+
+		JLabel lblDataDaOperao = new JLabel("Data da Operação:");
+		lblDataDaOperao.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblDataDaOperao.setBounds(357, 339, 190, 16);
+		JPCltTransf.add(lblDataDaOperao);
+
+		JButton btnConfirmar = new JButton("Confirmar");
+		btnConfirmar.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		btnConfirmar.setBounds(337, 435, 120, 38);
+		JPCltTransf.add(btnConfirmar);
+
+		JButton button_1 = new JButton("Limpar");
+		button_1.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		button_1.setBounds(509, 435, 120, 38);
+		JPCltTransf.add(button_1);
 
 		// Painel principal cliente
 		JPanel JPCltCM = new JPanel();
@@ -269,11 +452,6 @@ public class BancoAppClt {
 		textFieldCltCartao.setBounds(527, 94, 344, 30);
 		JPCltCM.add(textFieldCltCartao);
 
-		// Painel principal cartao
-		JPanel JPCltCartao = new JPanel();
-		JPCltCartao.setBounds(16, 16, 1032, 563);
-		JpanelPrincipal.add(JPCltCartao);
-
 		JTextField tbAdmFunPesq = new JTextField();
 		tbAdmFunPesq.setBounds(12, 52, 240, 30);
 		tbAdmFunPesq.setColumns(10);
@@ -286,7 +464,6 @@ public class BancoAppClt {
 		btCltConmov.setForeground(Color.BLACK);
 		btCltConmov.setBounds(0, 6, 170, 130);
 		btCltConmov.setBackground(new Color(188, 127, 82));
-		btCltConmov.setIcon(null);
 		JpanelMenu.add(btCltConmov);
 
 		// quando carrego no batao conta/movimentos , os outros paineis nao ficam
@@ -306,7 +483,6 @@ public class BancoAppClt {
 		// Botão transferencia do menu
 		JButton btCltTransferencia = new JButton("Transfer\u00EAncia");
 		btCltTransferencia.setBorderPainted(false);
-		btCltTransferencia.setBorder(UIManager.getBorder("Button.border"));
 		btCltTransferencia.setOpaque(true);
 		btCltTransferencia.setBackground(new Color(188, 127, 82));
 		btCltTransferencia.setBounds(0, 148, 170, 144);
@@ -316,9 +492,10 @@ public class BancoAppClt {
 		btCltTransferencia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
+				
+				JPCltTransf.setVisible(true);
 				JPCltCM.setVisible(false);
 				JPCltCartao.setVisible(false);
-				JPCltTransf.setVisible(true);
 				JPCltGestao.setVisible(false);
 			}
 		});
