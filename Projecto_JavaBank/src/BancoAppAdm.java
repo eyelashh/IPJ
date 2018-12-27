d3ab8d156fe16dc3cb794ea4e63beaeac150d66c
tamarabarros92 <tamarabarros@hotmail.com>
2018-12-27 09:37:54 +0000

    ttt

--- Projecto_JavaBank/src/BancoAppAdm.java
+++ Projecto_JavaBank/src/BancoAppAdm.java
@@ -102,6 +102,7 @@ public class BancoAppAdm {
 				try {
 
 					Login dialog = new Login();
+					
 
 					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 					dialog.setLocationRelativeTo(frame);
