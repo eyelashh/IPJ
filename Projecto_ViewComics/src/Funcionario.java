99d0dce559ec3a8233be151d3b56fbfefea762b6
Joana <joannapestanna@gmail.com>
2018-12-30 17:43:36 +0000

    AppAdminFunc

--- Projecto_ViewComics/src/Funcionario.java
+++ Projecto_ViewComics/src/Funcionario.java
@@ -9,8 +9,8 @@ public class Funcionario extends Utilizador implements  Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Funcionario(int id, String nome, int contato, String username, String password) {
-		super(id, nome, contato, username, password);
+	public Funcionario( String nome, int contato, String username, String password) {
+		super(nome, contato, username, password);
 		// TODO Auto-generated constructor stub
 
 	}
