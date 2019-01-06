d643e1a7a47f83d9870dbb3406f7654c8300a914
tamarabarros92 <tamarabarros@hotmail.com>
2019-01-06 20:39:37 +0000

    ttt

--- Projecto_JavaBank/src/BancoAppClt.java
+++ Projecto_JavaBank/src/BancoAppClt.java
@@ -293,6 +293,7 @@ public class BancoAppClt implements Serializable {
 					textFieldCltSaldoConta.setText(Double.toString(c.getSaldo()));
 
 					// cartao:
+					
 					Cartao card = gb.javabank.obterCartao(gb.javabank.getCartoes(), ((ContaCorrente) c).getCartao());
 
 					textFieldCltCartao.setText(card.getNomeTitular());
