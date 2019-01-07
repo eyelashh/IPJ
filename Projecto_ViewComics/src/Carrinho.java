0896075f36de859363c98983a45301ff03c66066
Joana <joannapestanna@gmail.com>
2019-01-07 14:54:59 +0000

    pulem por favor

--- Projecto_ViewComics/src/Carrinho.java
+++ Projecto_ViewComics/src/Carrinho.java
@@ -106,33 +106,7 @@ public class Carrinho implements Serializable {
 		return quantidade;
 
 	}
-//
-//	// listar hashmap para string
-//	public String[] listaCarrinho2() {
-//
-//		ArrayList<String> listaC = new ArrayList<String>();
-//		String a = "";
-//
-//
-//		// Criar um iterador para a key do hashmap para o poder percorrer
-//		// num foreach
-//		Set<Map.Entry<Integer, Integer>> set = this.conteudo.entrySet();
-//
-//		// percorrer o hm no foreach de modo a somar todos os pre�os
-//		for (Map.Entry<Integer, Integer> me : set) {
-//
-//			a = me.getKey() + " " + me.getValue();
-//			listaC.add(a);
-//
-//		}
-//		
-//		String[] listalivro = new String[listaC.size()];
-//		listalivro = listaC.toArray(listalivro);
-//
-//		return listalivro;
-//
-//	}
-	
+
 	public String[] listaCarrinho2() {
 
 		ArrayList<String> listaC = new ArrayList<String>();
