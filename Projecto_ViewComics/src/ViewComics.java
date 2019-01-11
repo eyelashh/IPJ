941ee313f1554d268e759ab46d65c601b37deb10
Joana <joannapestanna@gmail.com>
2019-01-11 15:53:04 +0000

    fff

--- Projecto_ViewComics/src/ViewComics.java
+++ Projecto_ViewComics/src/ViewComics.java
@@ -6,10 +6,39 @@ import javax.swing.JDialog;
 public class ViewComics implements  Serializable {
 
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
+	int counter = 1000;
+	Thread t1 = new Thread(new Runnable() {
+		//crio um objecto da classe cliente?
 		AppCliente cliente = new AppCliente();
-		cliente.run();
+		
+	//	log.run();
+		
+		@Override
+		public void run() {
+			//inicio um runnable para appCliente que criei??
+			cliente.run();
+			//show(counter, "New Thread!");
+		}
+	});
+
+	t1.start();
+
+	show(counter, "Main method!");
+}
+
+private static void show(int counter, String msg) {
+	
+	//ler o ficheiro partilhado
+	while (counter-- > 0) {
+		System.out.println(msg);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+}
 
 }
